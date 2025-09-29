7ad82615f161e0de9801377d97e4cf3da92d2a07
Maksym Prysyazhny <maksymprysyazhny@icloud.com>
2025-09-29 00:06:56 -0500

    tests

--- tests/where_test.cpp
+++ tests/where_test.cpp
@@ -8,3 +8,34 @@
 TEST_CASE( "it returns Hello World" ) {
     REQUIRE( hello() == "Hello World!" );
 }
+
+TEST_CASE( "it finds a substring in a string" ) {
+    REQUIRE( findString("hello", "lo") == 4 );
+    REQUIRE( findString("hello", "he") == 1 );
+    REQUIRE( findString("hello", "l") == 3 );
+    REQUIRE( findString("hello", "hello") == 1 );
+    REQUIRE( findString("hello", "world") == -1 );
+    REQUIRE( findString("hello", "") == 1 );
+}
+
+TEST_CASE( "it finds a character in a string" ) {
+    REQUIRE( findString("hello", 'h') == 1 );
+    REQUIRE( findString("hello", 'e') == 2 );
+    REQUIRE( findString("hello", 'l') == 3 );
+    REQUIRE( findString("hello", 'o') == 5 );
+    REQUIRE( findString("hello", 'z') == -1 );
+}
+
+TEST_CASE( "it works with single character strings" ) {
+    REQUIRE( findString("a", "a") == 1 );
+    REQUIRE( findString("a", "b") == -1 );
+    REQUIRE( findString("a", 'a') == 1 );
+    REQUIRE( findString("a", 'b') == -1 );
+}
+
+TEST_CASE( "it works with special characters" ) {
+    REQUIRE( findString("!@#$%^&*()", "@#") == 2 );
+    REQUIRE( findString("!@#$%^&*()", "a") == -1 );
+    REQUIRE( findString("!@#$%^&*()", '@') == 2 );
+    REQUIRE( findString("!@#$%^&*()", 'a') == -1 );
+}
